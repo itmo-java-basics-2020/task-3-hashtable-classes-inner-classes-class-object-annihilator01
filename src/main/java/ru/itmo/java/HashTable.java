d401c9290fe2a5cde6b863dba85684323612a7cf
Ilia Dubrovskii <ghrom07@yandex.ru>
2020-03-14 01:16:18 +0300

    Implemented HashTable class
    - Implemented .resize() method for array expansion
    - Added threshold variable for defining when using .resize()
    - Added constructor with loadFactor
    - Implemented all default methods

--- src/main/java/ru/itmo/java/HashTable.java
+++ src/main/java/ru/itmo/java/HashTable.java
@@ -1,23 +1,178 @@
 package ru.itmo.java;
 
-import java.util.Map;
+import java.util.Objects;
 
 public class HashTable {
+    static final int HASH_INTERVAL = 13;
+    static final int DEFAULT_INITIAL_CAPACITY = 16;
+    static final float DEFAULT_LOAD_FACTOR = 0.5f;
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    static class Entry {
+        Object key;
+        Object value;
+        boolean isReusable;
+
+        Entry(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+            this.isReusable = false;
+        }
+
+        public final Object getKey() {
+            return key;
+        }
+
+        public final Object getValue() {
+            return value;
+        }
+
+        public boolean isReusable() {
+            return isReusable;
+        }
+
+        public void setValue(Object value) {
+            this.value = value;
+        }
+
+        public void makeReusable() {
+            key = null;
+            value = null;
+            isReusable = true;
+        }
+    }
+
+    private int size;
+    private int capacity;
+    private float loadFactor;
+    private int threshold;
+    private Entry[] table;
+
+    public HashTable() {
+        this(DEFAULT_INITIAL_CAPACITY);
+    }
+
+    public HashTable(int initialCapacity) {
+        this(initialCapacity, DEFAULT_LOAD_FACTOR);
+    }
+
+    public HashTable(int initialCapacity, float loadFactor) {
+        if (initialCapacity <= 0) {
+            throw new IllegalArgumentException("Illegal initial capacity: " + initialCapacity);
+        }
+
+        if (initialCapacity > MAXIMUM_CAPACITY) {
+            initialCapacity = MAXIMUM_CAPACITY;
+        }
+
+        if (loadFactor <= 0 || Float.isNaN(loadFactor)) {
+            throw new IllegalArgumentException("Illegal load factor: " + loadFactor);
+        }
+
+        this.size = 0;
+        this.capacity = initialCapacity;
+        this.loadFactor = loadFactor;
+        this.threshold = (int) Math.ceil(capacity * loadFactor);
+        this.table = new Entry[initialCapacity];
+    }
+
+    private int mod(int x, int y) {
+        return Math.floorMod(x, y);
+    }
 
     Object put(Object key, Object value) {
-        throw new UnsupportedOperationException();
+        int hash = Objects.hashCode(key);
+        int reusableBucketIndex = -1;
+
+        int i;
+        for (i = 0; table[mod(hash + HASH_INTERVAL * i, capacity)] != null; i++) {
+            if (i > MAXIMUM_CAPACITY) {
+                throw new RuntimeException("Infinite collisions or no space to put one more entry");
+            }
+
+            if (reusableBucketIndex == -1 && table[mod(hash + HASH_INTERVAL * i, capacity)].isReusable()) {
+                reusableBucketIndex = mod(hash + HASH_INTERVAL * i, capacity);
+            }
+
+            if (Objects.equals(table[mod(hash + HASH_INTERVAL * i, capacity)].getKey(), key)) {
+                Object oldValue = table[mod(hash + HASH_INTERVAL * i, capacity)].getValue();
+                table[mod(hash + HASH_INTERVAL * i, capacity)].setValue(value);
+                return oldValue;
+            }
+        }
+
+        int putBucketIndex = (reusableBucketIndex == -1) ? mod(hash + HASH_INTERVAL * i, capacity)
+                : reusableBucketIndex;
+        table[putBucketIndex] = new Entry(key, value);
+
+        if (++size > threshold) {
+            resize();
+        }
+
+        return null;
     }
 
     Object get(Object key) {
-        throw new UnsupportedOperationException();
+        int hash = Objects.hashCode(key);
+
+        int i;
+        for (i = 0; table[mod(hash + HASH_INTERVAL * i, capacity)] != null; i++) {
+            if (i > MAXIMUM_CAPACITY) {
+                return null;
+            }
+
+            if (Objects.equals(table[mod(hash + HASH_INTERVAL * i, capacity)].getKey(), key)) {
+                return table[mod(hash + HASH_INTERVAL * i, capacity)].getValue();
+            }
+        }
+
+        return null;
     }
 
     Object remove(Object key) {
-        throw new UnsupportedOperationException();
+        int hash = Objects.hashCode(key);
+
+        int i;
+        for (i = 0; table[mod(hash + HASH_INTERVAL * i, capacity)] != null; i++) {
+            if (i > MAXIMUM_CAPACITY) {
+                return null;
+            }
+
+            if (Objects.equals(table[mod(hash + HASH_INTERVAL * i, capacity)].getKey(), key)) {
+                Object oldValue = table[mod(hash + HASH_INTERVAL * i, capacity)].getValue();
+                table[mod(hash + HASH_INTERVAL * i, capacity)].makeReusable();
+                --size;
+                return oldValue;
+            }
+        }
+
+        return null;
     }
 
     int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
+    private void resize() {
+        int oldCapacity = capacity;
+        Entry[] oldTable = table;
+
+        int newCapacity = oldCapacity * 2;
+        int newThreshold = (int) Math.ceil(newCapacity * loadFactor);
+        Entry[] newTable = new Entry[newCapacity];
+
+        capacity = newCapacity;
+        threshold = newThreshold;
+        table = newTable;
+        size = 0;
+
+        for (int i = 0; i < oldCapacity; i++) {
+            Entry entry = oldTable[i];
+            oldTable[i] = null;
+
+            if (entry != null && !entry.isReusable()) {
+                put(entry.getKey(), entry.getValue());
+            }
+        }
+    }
 }
